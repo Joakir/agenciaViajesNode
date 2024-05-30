118d9afd3dadb99e273233039dd3ebc869b281da
Marcos Aguilar <marcos_fail@hotmail.com>
2024-05-30 12:10:07 -0300

    Actualizaciones
    Agregando gitignore
    Agregando hojas de estilo y bootstrap
    Creado el Layout Principal
    Agregado PUG

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { // req - lo que enviamos || res - lo que express nos responde
-  res.send('Hola Mundo');
+  res.render('inicio');
 });
 
 router.get('/nosotros', (req, res) => {
-  res.send('Nosotros');
+  res.render('nosotros');
 });
 
-router.get('/Contacto', (req, res) => {
-  res.send('Contacto');
-});
+export default router;
