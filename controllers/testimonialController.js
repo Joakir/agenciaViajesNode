4a00db83c1c15824727469a9d7921ebd150d6257
Marcos Aguilar <marcos_fail@hotmail.com>
2024-06-19 02:35:46 -0300

    Updates

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,5 +1,6 @@
+import {Testimonial} from '../models/Testimoniales.js'
 
-const guardarTestimonial = (req, res) => {
+const guardarTestimonial = async (req, res) => {
   //Validar...
   const {nombre, correo, mensaje} = req.body;
 
@@ -15,7 +16,34 @@ const guardarTestimonial = (req, res) => {
     errores.push({mensaje: 'El Mensaje esta vacio'});
   }
 
-  console.log(errores);
+  if(errores.length > 0){
+
+    //Consultar Testimoniales Existentes
+    const testimoniales = await Testimonial.findAll();
+
+    //Mostrar la vista con errores
+    res.render('testimoniales', {
+      pagina: 'Testimoniales',
+      errores,
+      nombre,
+      correo,
+      mensaje,
+      testimoniales
+    })
+  } else {
+    //Alamcenarlo en la base de datos
+    try {
+      await Testimonial.create({
+        nombre,
+        correo,
+        mensaje
+      });
+
+      res.redirect('/testimoniales')
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ db.authenticate()
   .catch(error => console.log(error));
 
 //Definir Puerto
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 //Habilitar PUG
 app.set('view engine', 'pug');
